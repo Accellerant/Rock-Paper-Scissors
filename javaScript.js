62b7544c095b64a775a11ef563aa29de97d68a71
Accellerant <accellerant@gmail.com>
2022-03-04 15:40:45 -0800

    PseudoCode + Requirements

---
+++ javaScript.js
@@ -0,0 +1,45 @@
+/*  
+Required function/variables/parameters/etc. names:
+    function:  
+        computerPlay(), game(), playRound(), 
+
+    parameters: 
+        playerSelection, computerSelection
+
+
+
+    Pseudocode:
+        Three Game Variables for Rock, Paper, Scissors
+        Two Variables for User and CPU: Score and Choice
+        
+        #Program Starts
+        Game loops 5 times.
+            CPU randomly chooses one of three Game Variables
+            
+            User chooses Game variable via String
+                Convert to lower-case string.
+                Compare to three game variables.
+                None match? Repeat 'till valid user input.
+
+            Print Who Chose What
+
+            Compare CPU to User Choice.
+                If CPU Choice != User Choice
+                    Compare all plausible CPU win conditions
+                    If (  CPU == Rock, User == Scissors   OR  
+                        CPU == Paper, User == Rock  OR
+                        PU == Scissors, User == Paper   )
+
+                        CPU +1 Score 
+                        Print CPU Scored a point
+
+                    else CPU had no wins, then the User wins
+                        User +1 Score
+                        Print User Scored a point
+                    
+                otherwise, both must have the same choice
+                    print tie message
+        Exit Loop - Compare Scores
+        Declare Winner
+        #Program Ends
+*/
