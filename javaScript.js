506e25023b448e6120da0986e2c9f3c537f26594
Accellerant <accellerant@gmail.com>
2022-03-04 15:44:27 -0800

    Tidied Comments

--- javaScript.js
+++ javaScript.js
@@ -1,47 +1,46 @@
+//Requirements
 /*  
-Required function/variables/parameters/etc. names:
     function:  
         computerPlay(), game(), playRound(), 
 
     parameters: 
         playerSelection, computerSelection
-
-
 */
 
+// Pseudocode
 /*
-    Pseudocode:
-        Three Game Variables for Rock, Paper, Scissors
-        Two Variables for User and CPU: Score and Choice
+    Three Game Variables for Rock, Paper, Scissors
+    Two Variables for User and CPU: Score and Choice
+    
+    #Program Starts
+    Game loops 5 times.
+        CPU randomly chooses one of three Game Variables
         
-        #Program Starts
-        Game loops 5 times.
-            CPU randomly chooses one of three Game Variables
-            
-            User chooses Game variable via String
-                Convert to lower-case string.
-                Compare to three game variables.
-                None match? Repeat 'till valid user input.
+        User chooses Game variable via String
+            Convert to lower-case string.
+            Compare to three game variables.
+            None match? Repeat 'till valid user input.
 
-            Print Who Chose What
+        Print Who Chose What
 
-            Compare CPU to User Choice.
-                If CPU Choice != User Choice
-                    Compare all plausible CPU win conditions
-                    If (  CPU == Rock, User == Scissors   OR  
-                        CPU == Paper, User == Rock  OR
-                        PU == Scissors, User == Paper   )
+        Compare CPU to User Choice.
+            If CPU Choice != User Choice
+                Compare all plausible CPU win conditions
+                If (  CPU == Rock, User == Scissors   OR  
+                    CPU == Paper, User == Rock  OR
+                    PU == Scissors, User == Paper   )
 
-                        CPU +1 Score 
-                        Print CPU Scored a point
+                    CPU +1 Score 
+                    Print CPU Scored a point
 
-                    else CPU had no wins, then the User wins
-                        User +1 Score
-                        Print User Scored a point
-                    
-                otherwise, both must have the same choice
-                    print tie message
-        Exit Loop - Compare Scores
-        Declare Winner
-        #Program Ends
+                else CPU had no wins, then the User wins
+                    User +1 Score
+                    Print User Scored a point
+                
+            otherwise, both must have the same choice
+                print tie message
+    Exit Loop - Compare Scores
+    Declare Winner
+    #Program Ends
 */
+
